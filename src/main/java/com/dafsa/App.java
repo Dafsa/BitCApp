de8c4535cca5de6b59497df5f0566772aa00598e
Rodrigo Baquero <rodrigo.baquero@gmail.com>
2017-04-23 17:02:15 +0200

    First commit.

---
+++ src/main/java/com/dafsa/App.java
@@ -0,0 +1,23 @@
+package com.dafsa;
+
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * Main class: Entry point of the application.
+ *
+ * @author Dafsa
+ *
+ */
+public class App 
+{
+
+    private static final Logger logger = Logger.getLogger(App.class);
+
+    public static void main( String[] args )
+    {
+        logger.info("Starting BitCApp");
+
+
+    }
+}
