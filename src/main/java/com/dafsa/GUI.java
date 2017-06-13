92df0ee576685aefa1fd3bcef9b494a2408ad490
Rodrigo Baquero <rodrigo.baquero@gmail.com>
2017-06-13 23:01:59 +0200

    Add getting orders every minute.

--- src/main/java/com/dafsa/GUI.java
+++ src/main/java/com/dafsa/GUI.java
@@ -11,13 +11,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
 
 /**
  *
  * @author Dafsa
  */
 public class GUI extends Application {
-    
+
+    private static final Logger logger = Logger.getLogger(GUI.class);
+
+    public static void onUpdateOrders (List<BigDecimal> orders ) {
+        //Upudate the chart with JavaFX!!!!!!
+        //TODO David
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("GUI.fxml"));
@@ -33,6 +45,41 @@ public class GUI extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+
+        logger.info("Starting BitCApp");
+
+        Runnable updateOrdersRunnable = new Runnable() {
+            @Override
+            public void run() {
+
+                //TODO Fix better stop mechanism from Java FX
+                while (true) {
+
+                    try {
+
+                        logger.info("Getting new orders....");
+
+                        List<BigDecimal> orders = BitCoinService.getOrders();
+                        onUpdateOrders(orders);
+                        Thread.sleep(1000 * 60);
+
+                        logger.info("New orders received.");
+
+                    } catch (InterruptedException | IOException e) {
+                        logger.error(e);
+                    }
+
+
+                }
+
+            }
+        };
+
+
+        Thread ordersThread = new Thread(updateOrdersRunnable);
+        ordersThread.start();
+
+
     }
     
 }
